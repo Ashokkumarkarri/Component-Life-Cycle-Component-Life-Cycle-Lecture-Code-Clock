03c0cd48c8a68c532cc68f4a1a7b7b37cb5af36e
Ashok kumar <ashok.ashok1999@gmail.com>
2025-05-05 00:50:58 +0000

    added toggle logic

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Clock from './components/Clock'
 import './App.css'
 
 class App extends Component {
+  state = {showclock: false}
+
+  onTOggleClock = () => {
+    this.setState(prevState => {
+      const {showclock} = prevState
+      return {showclock: !showclock}
+    })
+  }
+
   render() {
+    const {showclock} = this.state
     return (
       <div className="app-container">
-        <button type="button" className="toggle-button">
-          Hide Clock
+        <button
+          type="button"
+          className="toggle-button"
+          onClick={this.onTOggleClock}
+        >
+          {showclock ? 'Hide Clock' : 'Show Clock'}
         </button>
-        <Clock />
+        {showclock && <Clock />}
       </div>
     )
   }
